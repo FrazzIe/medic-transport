f8b168b88cef7a9d4ec8eea34d150811c9ab5ea8
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-02-28 14:32:05 +0000

    feat(death): onClientResourceStart listener

--- cl_death.js
+++ cl_death.js
@@ -3,11 +3,20 @@
 */
 
 /**
+ * Event listener called when a resource has been started
+ * @param {string} resource name of resource
+ */
+function resourceStarted(resource)
+{
+
+}
+
+/**
  * Init event listeners & vars
  */
 function init()
 {
-
+	on("onClientResourceStart", resourceStarted);
 }
 
 init();
