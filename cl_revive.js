caedc9643afef7d86bc8ba22864046b5d7dea5e7
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-14 16:53:32 +0100

    feat(revive): onRevivePlayer handler

--- cl_revive.js
+++ cl_revive.js
@@ -1,1 +1,21 @@
 /// <reference path="node_modules/@citizenfx/client/index.d.ts"/>
+
+/*
+	Revive handler
+*/
+
+/**
+ * Event listener called when a player is to be revived
+ */
+function onRevivePlayer()
+{
+
+}
+
+/**
+ * Init event listeners & vars
+ */
+function init()
+{
+	onNet("revivePlayer", onResourceStarted);
+}
