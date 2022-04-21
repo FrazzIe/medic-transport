86c7bc9a2e7cfea7feda644254ce899ba5aa5017
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-21 09:38:59 +0100

    feat(rescue/sh): share RESCUE_TYPES enum

--- cl_rescue.js
+++ cl_rescue.js
@@ -5,16 +5,6 @@
 */
 
 /**
- * Improvised Enum of available types of rescues
- */
-const RESCUE_TYPES =
-{
-	NONE: 0,
-	GROUND: 1,
-	AIR: 2
-};
-
-/**
  * List of hospital delivery locations
  */
 const RESCUE_HOSPITALS =
---
+++ sh_rescue.js
@@ -0,0 +1,16 @@
+/// <reference path="node_modules/@citizenfx/client/index.d.ts"/>
+/// <reference path="node_modules/@citizenfx/server/index.d.ts"/>
+
+/*
+	Manage player rescue
+*/
+
+/**
+ * Improvised Enum of available types of rescues
+ */
+const RESCUE_TYPES =
+{
+	NONE: 0,
+	GROUND: 1,
+	AIR: 2
+};
