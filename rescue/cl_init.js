a47f3d77a49db0c1612fa93265d4952b268b2bf7
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-22 20:48:29 +0100

    feat(rescue): split up into config & init file

--- rescue/cl_init.js
+++ rescue/cl_init.js
@@ -28,81 +28,6 @@ const RESCUE_STAGES =
 };
 
 /**
- * List of hospital delivery locations
- */
-const RESCUE_HOSPITALS =
-[
-	[ 340.73, -560.04, 28.74 ],   // DOWNT
-	[ -492.00, -336.81, 34.37 ],  // ROCKF
-	[ 298.50, -1442.30, 29.79 ],  // DAVIS
-	[ 1153.37, -1512.34, 34.69 ], // EBURO
-	[ 1827.27, 3693.52, 34.22 ],  // SANDY
-	[ -237.30, 6332.01, 32.40 ]   // PALETO
-]
-
-/**
- * How far away a rescue should be spawned from death location
- * 
- * GTA Metres
- */
-const RESCUE_SPAWN_DIST = 300.0;
-
-/**
- * List of zones that are unreachable
- */
-const RESCUE_ZONE_BLACKLIST = [ "ARMYB", "JAIL" ];
-
-/**
- * List of zones that can only be reached by air
- */
-const RESCUE_ZONE_AIR = [ "PALCOV", "OCEANA" ];
-
-/**
- * List of interior hashes that are reachable
- */
-const RESCUE_INTERIOR_WHITELIST =
-[
-	1847849587, // AIRPORT HANGAR
-	1932111343, // ROGERS SALVAGE & SCRAP
-	-1760196495, // BARBER SHOP
-	-1738793216, // AMMU-NATION SMALL
-	-26658153, // AMMU-NATION BIG
-	371397885, // MAZEBANK GARAGE 01
-	-1710765799, // MAZEBANK GARAGE 02
-	1753457757, // BINCO CLOTHING
-	225974099, // SUBURBAN CLOTHING
-	-1690750853, // 24/7 STORE
-	-945079088, // ROCKFORD PLAZA
-	-429983331, // PACIFIC STANDARD BANK
-	1338737678, // FLEECA BANK
-	607268480, // BLAINE COUNTY SAVINGS BANK
-	-1716756720, // ICE PLANET JEWELRY
-	1457285595, // TEQUI-LA-LA
-	-298980389, // PONSONBYS
-	1724779208, // DEL PERRO BEACH APT.
-	-1407739860, // MISSION ROW POLICE DEPARTMENT
-	-371481864, // PALETO SHERIFFS OFFICE
-	-400174009, // SANDY SHERIFFS OFFICE
-	-89256394, // LS CUSTOMS 01
-	100117129, // LS CUSTOMS 02
-	-1070602979, // LS CUSTOMS 03
-	299191145, // BENNY'S ORIGINAL MOTORWORKS
-	1314234446, // VANILLA UNICORN
-	-1480564353, // TATTOO SHOP
-	-1437859694, // PALETO TUNNEL 01
-	560393926, // PALETO TUNNEL 02
-	249579961, // PALETO TUNNEL 03
-	2136268782, // STRAWBERRY TUNNEL 01
-	260467921, // STRAWBERRY TUNNEL 02
-	-1935705469, // STRAWBERRY TUNNEL 03
-	604839936, // UNION DEPOSITORY
-	-1514137385, // UNDERGROUND PARKING
-	// VANGELICO JEWELRY
-	// SWEAT SHOP
-	// PREMIUM DELUXE MOTORSPORT
-];
-
-/**
  * Minimum length between bottom and top height map needed to perform an elevated position check
  * 
  * GTA Metres 0 - ...
@@ -164,48 +89,11 @@ const RESCUE_COLLISION_OFFSET = 100.0;
 const RESCUE_RAPPEL_HEIGHT = 10.0;
 
 /**
- * Available rescue vehicles for each rescue type
- */
-const RESCUE_VEHICLES =
-{
-	GROUND: "ambulance",
-	AIR: "polmav"
-};
-
-/**
- * Available rescue drivers for each rescue type
- */
-const RESCUE_DRIVER =
-{
-	GROUND: "s_m_m_paramedic_01",
-	AIR: "s_m_m_pilot_02"
-};
-
-/**
- * Available rescue passengers for each rescue type
- */
-const RESCUE_PASSENGER = 
-{
-	GROUND: 
-	{
-		model: "s_m_m_paramedic_01",
-		seat: 0,
-		count: 1
-	},
-	AIR:
-	{
-		model: "s_m_m_paramedic_01",
-		seat: 1,
-		count: 2
-	}
-};
-
-/**
  * Speed at which vehicles go when travelling to a point
  * 
  * MPH
  */
-const RESCUE_SPEED =
+const RESCUE_VEHICLE_SPEED =
 {
 	GROUND: 55,
 	AIR: 90
@@ -214,7 +102,7 @@ const RESCUE_SPEED =
 /**
  * Styles of driving used by rescue vehicle drivers
  */
-const RESCUE_STYLES = 
+const RESCUE_VEHICLE_STYLE = 
 {
 	GROUND: 262148,
 	AIR: 262144
@@ -454,7 +342,7 @@ function getEndPoint(pos)
 async function createRescueVehicle(point, rescueType)
 {
 	// get vehicle model
-	const model = RESCUE_VEHICLES[rescueType];
+	const model = RESCUE_VEHICLE[rescueType];
 
 	// load vehicle model
 	const loaded = await loadModel(model);
@@ -611,14 +499,15 @@ async function startRescue()
 		return;
 	}
 
+
 	// get vehicle driver
 	const driver = personnel[0];
 	// get vehicle travelling speed
-	const speed = RESCUE_SPEED[rescueType] / MPH_OFFSET;
+	const speed = RESCUE_VEHICLE_SPEED[rescueType] / MPH_OFFSET;
 	// get vehicle model
-	const model = RESCUE_VEHICLES[rescueType];
+	const model = RESCUE_VEHICLE[rescueType];
 	// get driving style
-	const style = RESCUE_STYLES[rescueType];
+	const style = RESCUE_VEHICLE_STYLE[rescueType];
 
 	// drive vehicle to point
 	TaskVehicleDriveToCoord(driver, vehicle, endPoint[0], endPoint[1], endPoint[2], speed, 0, model, style, RESCUE_VEHICLE_STOP_RANGE, 0.0);
@@ -641,3 +530,8 @@ function init()
 }
 
 init();
+
+RegisterCommand("meep", (src, args, raw) =>
+{
+	startRescue();
+});
