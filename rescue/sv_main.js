ffafb413f6e9d4acdd2e634d9f0745a90e61f3d4
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-24 22:10:09 +0100

    feat(rescue/sv): rescue map

---
+++ rescue/sv_main.js
@@ -0,0 +1,13 @@
+/// <reference path="node_modules/@citizenfx/server/index.d.ts"/>
+
+/*
+	Manage and track all rescues
+*/
+
+/**
+ * Map of rescues
+ * 
+ * key: server id
+ * value: rescue obj
+ */
+const rescues = {};
