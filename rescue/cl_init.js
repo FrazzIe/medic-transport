e04de463e86f45867e0fd8f7868e63dc52f590ae
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-24 22:45:33 +0100

    feat(rescue/sh): type, status, stage enums

--- rescue/cl_init.js
+++ rescue/cl_init.js
@@ -5,42 +5,6 @@
 */
 
 /**
- * Improvised Enum of available types of rescues
- */
-const RESCUE_TYPES =
-{
-	NONE: "NONE",
-	GROUND: "GROUND",
-	AIR: "AIR"
-};
-
-/**
- * Improvided Enum of rescue stages
- */
-const RESCUE_STAGES =
-{
-	GET_TYPE: "GET_TYPE",
-	GET_POINTS: "GET_POINTS",
-	AI_CREATE: "AI_CREATE",
-	AI_EXIT_VEHICLE: "AI_EXIT_VEHICLE",
-	AI_GOTO_POINT: "AI_GOTO_POINT",
-	AI_GRAB_PLAYER: "AI_GRAB_PLAYER",
-	AI_STORE_PLAYER: "AI_STORE_PLAYER",
-	AI_ENTER_VEHICLE: "AI_ENTER_VEHICLE",
-	VEHICLE_GOTO_POINT: "VEHICLE_GOTO_POINT"
-};
-
-/**
- * Improvided Enum of rescue status
- */
-const RESCUE_STATUS =
-{
-	FAILED: "FAILED",
-	ONGOING: "ONGOING",
-	COMPLETE: "COMPLETE"
-};
-
-/**
  * Try to rescue a downed player and bring them to the hospital for revival
  */
 async function startRescue()
--- rescue/sh_main.js
+++ rescue/sh_main.js
@@ -5,3 +5,42 @@
 	Shared rescue specific variables and functions
 */
 
+/**
+ * Improvised Enum of available types of rescues
+ */
+const RESCUE_TYPE =
+{
+	NONE: "NONE",
+	GROUND: "GROUND",
+	AIR: "AIR"
+};
+
+/**
+ * Improvided Enum of rescue status
+ */
+const RESCUE_STATUS =
+{
+	FAILED: "FAILED",
+	ONGOING: "ONGOING",
+	COMPLETE: "COMPLETE"
+};
+
+/**
+ * Improvided Enum of rescue stages
+ */
+const RESCUE_STAGE =
+{
+	GET_TYPE: "GET_TYPE",
+	GET_POINTS: "GET_POINTS",
+	AI_CREATE: "AI_CREATE",
+	AI_GOTO_POINT: "AI_GOTO_POINT",
+	AI_ENTER_VEHICLE: "AI_ENTER_VEHICLE",
+	AI_EXIT_VEHICLE: "AI_EXIT_VEHICLE",
+	AI_RAPPEL_VEHICLE: "AI_RAPPEL_VEHICLE",
+	AI_ATTACH_OBJECT: "AI_ATTACH_OBJECT",
+	VEHICLE_CREATE: "VEHICLE_CREATE",
+	VEHICLE_GOTO_POINT: "VEHICLE_GOTO_POINT",
+	VEHICLE_ATTACH_OBJECT: "VEHICLE_ATTACH_OBJECT",
+	OBJECT_CREATE: "OBJECT_CREATE",
+	OBJECT_ATTACH_PLAYER: "OBJECT_ATTACH_PLAYER"
+};
