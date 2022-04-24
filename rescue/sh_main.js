64d9e8435f7d78f33b31a90c806c8f4cd3f4aee3
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-24 22:45:06 +0100

    feat(rescue/sh): empty sh_main.js

---
+++ rescue/sh_main.js
@@ -0,0 +1,7 @@
+/// <reference path="node_modules/@citizenfx/client/index.d.ts"/>
+/// <reference path="node_modules/@citizenfx/server/index.d.ts"/>
+
+/*
+	Shared rescue specific variables and functions
+*/
+
