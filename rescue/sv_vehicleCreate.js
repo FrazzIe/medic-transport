4a781a25be8251b2f299e3cc8833c5cb2ba7eb71
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-04-28 15:08:30 +0100

    feat(rescue/vehicleCreate): create vehicle testing

--- rescue/sv_vehicleCreate.js
+++ rescue/sv_vehicleCreate.js
@@ -11,9 +11,25 @@
  * @param {object} rescue
  * @param {number | string} src   
  */
-function onStageInit(rescue, src)
+async function onStageInit(rescue, src)
 {
+	// get vehicle model
+	const model = RESCUE_VEHICLE[rescue.type];
 
+	// create vehicle
+	const vehicle = CreateVehicle(model, rescue.points.start[0], rescue.points.start[1], rescue.points.start[2], rescue.points.start[3], true, false);	
+
+	console.log(vehicle);
+	console.log(DoesEntityExist(vehicle));
+
+	while (!DoesEntityExist(vehicle))
+	{
+		console.log(DoesEntityExist(vehicle), "waiting", model, rescue.points.start);
+		await new Promise(res => setTimeout(res, 0));
+	}
+
+	console.log(NetworkGetNetworkIdFromEntity(vehicle));
+	console.log(NetworkGetEntityOwner(vehicle));
 }
 
 /**
