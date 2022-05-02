f7a02f16b66e027d5bb3e9a8d5cb888027486c0c
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-05-02 22:07:42 +0100

    feat(rescue/safety): add timeout to DoesEntityExist check

--- rescue/cl_safety.js
+++ rescue/cl_safety.js
@@ -3,7 +3,12 @@
 /*
 	Contains safety functions used
 	to ensure specifc conditions are met
-/*
+*/
+
+/**
+ * The number of seconds an entity must be non-existant before forcing a timeout
+ */
+const ENTITY_EXIST_TIMEOUT = 60;
 
 /**
  * Performs safety checks to ensure the rescue peds are viable
@@ -13,12 +18,36 @@ function pedSafetyCheck(peds)
 {
 	return new Promise(async (resolve, reject) => 
 	{
+		const timeouts = {};
+
 		while (true)
 		{
 			for (let i = 0; i < peds; i++)
 			{
 				const netId = peds[i];
 				const ped = NetworkGetEntityFromNetworkId(netId);
+
+				// entity exist?
+				if (!DoesEntityExist(ped))
+				{
+					// start exist timeout
+					if (!timeouts[netId])
+					{
+						timeouts[netId] = 0;
+					}
+
+					// inc timeout
+					timeouts[netId]++;
+
+					// has entity reached timeout threshold
+					if (timeouts[netId] >= ENTITY_EXIST_TIMEOUT)
+					{
+						reject();
+					}
+
+					// skip further checks
+					continue;
+				}
 			}
 		}
 	});
