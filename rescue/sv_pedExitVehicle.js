373d1b512805b8326e49df48b3db0e6a2839c12e
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-05-06 14:55:44 +0100

    feat(rescue/pedExitVehicle): pedExitVehicle func

--- rescue/sv_pedExitVehicle.js
+++ rescue/sv_pedExitVehicle.js
@@ -7,6 +7,33 @@
 */
 
 /**
+ * Make ped exit vehicle
+ * @param {number} pedNetId 
+ * @param {number} vehicleNetId 
+ * @returns {bool}
+ */
+async function pedExitVehicle(pedNetId, vehicleNetId)
+{
+	while (true)
+	{
+		// get ped & vehicle handles
+		const ped = NetworkGetEntityFromNetworkId(netId);
+		const vehicle = NetworkGetEntityFromNetworkId(netId);
+
+		// skip non-existant entities
+		// safety check will catch this
+		if (!DoesEntityExist(ped) || !DoesEntityExist(vehicle))
+		{
+			continue;
+		}
+
+		// TODO: Exit vehicle
+
+		await delay(0);
+	}
+}
+
+/**
  * Initiate stage
  * @param {object} rescue
  * @param {number | string} src   
