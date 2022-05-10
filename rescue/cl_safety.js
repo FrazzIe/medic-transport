8dae5175ebadcd514d23513dff5860f53da41a5e
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-05-10 15:07:24 +0100

    feat(rescue/safety/cl): entityDuplicityCheck handler
    Perform client-sided checks

--- rescue/cl_safety.js
+++ rescue/cl_safety.js
@@ -4,3 +4,30 @@
 	Contains safety functions used
 	to ensure specifc conditions are met
 */
+
+/**
+ * Perform runtime specific entity checks within entitySafetyCheck
+ * @param {number} ent entity handle
+ */
+function entityDuplicityCheck(ent)
+{
+	// vehicle specific checks
+	if (IsEntityAVehicle(ent))
+	{
+		// check if vehicle has somehow flipped
+		if (IsVehicleStuckOnRoof(ent))
+		{
+			throw new Error(`Entity with network id ${netId} has flipped`);
+		}
+	}
+
+	// ped specific checks
+	if (IsEntityAPed(ent))
+	{
+		// ensure ped is in a vehicle
+		if (!IsPedSittingInAnyVehicle(ent))
+		{
+			throw new Error(`Entity with network id ${netId} isn't in a vehicle`);
+		}
+	}
+}
