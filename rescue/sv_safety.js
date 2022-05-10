b20bd3f27ef4bde61c60dadeeb196ef4ed4c92ba
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-05-10 15:07:39 +0100

    feat(rescue/safety/sv): entityDuplicityCheck handler

---
+++ rescue/sv_safety.js
@@ -0,0 +1,15 @@
+/// <reference path="node_modules/@citizenfx/server/index.d.ts"/>
+
+/*
+	Contains safety functions used
+	to ensure specifc conditions are met
+*/
+
+/**
+ * Perform runtime specific entity checks within entitySafetyCheck
+ * @param {number} ent entity handle
+ */
+function entityDuplicityCheck(ent)
+{
+
+}
